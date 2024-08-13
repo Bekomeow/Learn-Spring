28aa9d7dc851ab9677f8d77e093475557efcb5c9
Beko <gabitcool31@gmail.com>
2024-08-13 18:31:51 +0600

    first

--- src/main/java/org/beko/spring/database/pool/ConnectionPool.java
+++ src/main/java/org/beko/spring/database/pool/ConnectionPool.java
@@ -7,7 +7,7 @@ public class ConnectionPool {
     private final String username;
     private final Integer poolSize;
     private final List<Object> args;
-    private final Map<String, Object> properties;
+    private Map<String, Object> properties;
 
     public ConnectionPool(String username, Integer poolSize, List<Object> args, Map<String, Object> properties) {
         this.username = username;
@@ -15,4 +15,9 @@ public class ConnectionPool {
         this.args = args;
         this.properties = properties;
     }
+
+    public void setProperties(Map<String, Object> properties) {
+        this.properties = properties;
+    }
 }
+
